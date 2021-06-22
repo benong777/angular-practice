36e10507d130e50d966ab3fbfaedee02e65f9939
Ben Ong <ben_ong@yahoo.com>
2021-06-22 16:00:07 -0700

    Added remove and add buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-practice';
+
+  firstName = '';
+  lastName = '';
+  year: number = 0;
+
+  persons = [
+    {
+      fName: "Joe",
+      lName: "Montana",
+      year: 1990
+    },
+    {
+      fName: "Steve",
+      lName: "Young",
+      year: 1995
+    },
+    {
+      fName: "Tom",
+      lName: "Brady",
+      year: 2000
+    }
+  ];
+
+  
+  addPerson() {
+    this.persons.push({
+      fName: this.firstName, 
+      lName: this.lastName,
+      year: this.year});
+    
+    this.firstName = '';
+    this.lastName = '';
+    this.year = 0;
+  }
+
+  removePerson(index: number) {
+    this.persons.splice(index, 1);
+  }
 }
